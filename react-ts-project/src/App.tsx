679ff45acfa85756555c47fee4241d1a7e491294
jydnbach <jaydenkim95@gmail.com>
2024-07-04 17:14:16 +0900

    todos setup

--- react-ts-project/src/App.tsx
+++ react-ts-project/src/App.tsx
@@ -1,7 +1,12 @@
+import Todos from './components/Todo';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
