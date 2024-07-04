50cf5e25efa83432d9ca0cd6f5dbfad8a1e405e1
jydnbach <jaydenkim95@gmail.com>
2024-07-04 15:55:35 +0900

    base types

---
+++ basics.ts
@@ -0,0 +1,14 @@
+// primitives: nums, string, boolean
+// complex: arrays, objects
+// fn, params
+
+// primitives
+
+let age: number;
+age = 12;
+
+let userName: string;
+userName = 'Max';
+
+let isInstructor: boolean;
+isInstructor = true;
