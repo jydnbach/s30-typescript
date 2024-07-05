7e0b17ed56e5c24c6fda36277b1ba46a79764b96
jydnbach <jaydenkim95@gmail.com>
2024-07-05 15:46:52 +0900

    useref typescript for form submithandler

--- react-ts-project/src/components/NewTodo.tsx
+++ react-ts-project/src/components/NewTodo.tsx
@@ -1,12 +1,22 @@
+import { useRef } from 'react';
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <lable htmlFor="text">Todo text</lable>
-      <input type="text" id="text" />
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
